594b81377e4b92742690d8be8e758753d9f6bfc0
Jurek <jurek2006@gmail.com>
2019-03-20 06:02:00 +0100

    xlsx export test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import * as moment from "moment";
+import { ExcelService } from "./services/excel.service";
 
 @Component({
   selector: "app-root",
@@ -10,5 +11,30 @@ export class AppComponent {
   title = "angular-contract-hours";
   date = moment("1995-12-25");
 
-  daysInMonth = moment("2019-03", "YYYY-MM").daysInMonth(); // 29
+  daysInMonth = moment("2019-03", "YYYY-MM").daysInMonth();
+
+  name = "Angular 6";
+  data: any = [
+    {
+      eid: "e101",
+      ename: "Jurek",
+      esal: 1000
+    },
+    {
+      eid: "e102",
+      ename: "ram",
+      esal: 2000
+    },
+    {
+      eid: "e103",
+      ename: "rajesh",
+      esal: 3000
+    }
+  ];
+
+  constructor(private excelService: ExcelService) {}
+
+  exportAsXLSX(): void {
+    this.excelService.exportAsExcelFile(this.data, "sample");
+  }
 }
