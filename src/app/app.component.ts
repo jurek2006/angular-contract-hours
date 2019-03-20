52715b743e61c86549da96fef998e94eb920fe1e
Jurek <jurek2006@gmail.com>
2019-03-20 05:47:10 +0100

    moment test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-contract-hours';
+  title = "angular-contract-hours";
+  date = moment("1995-12-25");
+
+  daysInMonth = moment("2019-03", "YYYY-MM").daysInMonth(); // 29
 }
