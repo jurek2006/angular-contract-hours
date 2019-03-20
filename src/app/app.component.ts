ed00fb0b3f135ebdacf88f2a6b804430c2a249ef
Jurek <jurek2006@gmail.com>
2019-03-20 10:14:38 +0100

    generating month days

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import * as moment from "moment";
 import { ExcelService } from "./services/excel.service";
 
@@ -7,34 +7,47 @@ import { ExcelService } from "./services/excel.service";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "angular-contract-hours";
-  date = moment("1995-12-25");
+  date = moment("2019-03-01");
 
-  daysInMonth = moment("2019-03", "YYYY-MM").daysInMonth();
+  days = [];
 
-  name = "Angular 6";
-  data: any = [
-    {
-      eid: "e101",
-      ename: "Jurek",
-      esal: 1000
-    },
-    {
-      eid: "e102",
-      ename: "ram",
-      esal: 2000
-    },
-    {
-      eid: "e103",
-      ename: "rajesh",
-      esal: 3000
-    }
-  ];
+  // name = "Angular 6";
+  // data: any = [
+  //   {
+  //     eid: "e101",
+  //     ename: "Jurek",
+  //     esal: 1000
+  //   },
+  //   {
+  //     eid: "e102",
+  //     ename: "ram",
+  //     esal: 2000
+  //   },
+  //   {
+  //     eid: "e103",
+  //     ename: "rajesh",
+  //     esal: 3000
+  //   }
+  // ];
 
   constructor(private excelService: ExcelService) {}
 
+  ngOnInit() {
+    const startDay = moment("2019-03-01");
+    for (let i = 0; i < startDay.daysInMonth(); i++) {
+      const day = startDay.clone().add(i, "days");
+
+      this.days.push({
+        date: day.format("D-MM"),
+        hours: day.day() === 6 || day.day() === 0 ? 0 : 1
+      });
+    }
+    console.log(this.days);
+  }
+
   exportAsXLSX(): void {
-    this.excelService.exportAsExcelFile(this.data, "sample");
+    this.excelService.exportAsExcelFile(this.days, "sample");
   }
 }
