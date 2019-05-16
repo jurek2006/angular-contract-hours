78fc7a56bb84513a370c08b5f0b3f3d260daa5f8
Jurek <jurek2006@gmail.com>
2019-05-16 09:51:46 +0200

    very rough pdf generatin

--- src/app/schedule/schedule-print/schedule-print.component.ts
+++ src/app/schedule/schedule-print/schedule-print.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 import { ScheduleDay } from "../scheduleDay.model";
+import * as jspdf from "jspdf";
+import html2canvas from "html2canvas";
 
 @Component({
   selector: "app-schedule-print",
@@ -18,5 +20,30 @@ export class SchedulePrintComponent implements OnInit {
 
   onClose() {
     this.closePrint.emit();
+  }
+
+  public generatePdf() {
+    let data = document.getElementById("contentToConvert");
+    console.log("data", data);
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      // var imgWidth = canvas.height / 20;
+      // var imgHeight = (canvas.height * imgWidth) / canvas.width;
+      console.log("canvas.height", canvas.height);
+      console.log("canvas.width", canvas.width);
+      const imgHeight = 290;
+
+      const page = { height: 297, width: 210 };
+      const aspect = page.height / canvas.height;
+      const imgWidth = aspect * canvas.width;
+      const marginLeft = (page.width - imgWidth) / 2;
+
+      const contentDataURL = canvas.toDataURL("image/jpeg", 0.5);
+      let pdf = new jspdf("p", "mm", "a4"); // A4 size page of PDF
+      const position = 0;
+      console.log("contentDataURL", contentDataURL);
+      pdf.addImage(contentDataURL, "PNG", marginLeft, 0, imgWidth, page.height);
+      pdf.save("MYPdf.pdf"); // Generated PDF
+    });
   }
 }
