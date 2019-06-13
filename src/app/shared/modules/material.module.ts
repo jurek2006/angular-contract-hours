90cd27dd1711908db0978e5872e3b0474244a817
Jurek <jurek2006@gmail.com>
2019-06-13 09:15:15 +0200

    added checkbox

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -5,16 +5,18 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material";
 import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+
+const matModules = [
+  MatButtonModule,
+  MatInputModule,
+  MatSelectModule,
+  MatCheckboxModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatInputModule,
-    MatSelectModule
-  ],
-  exports: [MatButtonModule, MatInputModule, MatSelectModule]
+  imports: [CommonModule, BrowserAnimationsModule, ...matModules],
+  exports: [...matModules]
 })
 export class MaterialModule {}
