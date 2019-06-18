8b6d41aa846e43c334a90abf875da65d9d4ecb89
Jurek <jurek2006@gmail.com>
2019-06-18 11:49:09 +0200

    refactor - phase 1

--- src/app/schedule/schedule-settings/schedule-settings.component.ts
+++ src/app/schedule/schedule-settings/schedule-settings.component.ts
@@ -12,6 +12,8 @@ import { Month } from "src/app/shared/month";
 import { Moment } from "moment";
 import { Subject } from "rxjs";
 import { takeUntil } from "rxjs/operators";
+import { settings } from "cluster";
+import moment = require("moment");
 
 @Component({
   selector: "app-schedule-settings",
@@ -20,22 +22,28 @@ import { takeUntil } from "rxjs/operators";
 })
 export class ScheduleSettingsComponent implements OnInit, OnDestroy {
   private ngUnsubscribe = new Subject();
-  monthPickerForm: FormGroup;
-  contractorForm: FormGroup;
+  // monthPickerForm: FormGroup; // TEMP - wywalić
+  // contractorForm: FormGroup; // TEMP - wywalić
+  settingsForm: FormGroup;
   months: Month[];
+  areSettingsSubmitted = false;
+  contractorName: string;
 
-  @Input() isMonthSubmitted: boolean;
-  @Output() isMonthSubmittedChange = new EventEmitter<boolean>();
-  @Input() selectedMonth: Moment;
-  @Output() selectedMonthChange = new EventEmitter<string>();
-  @Input() contractorName: string;
-  @Output() contractorNameChange = new EventEmitter<string>();
+  @Input() settings: any;
+  @Output() settingsChange = new EventEmitter<any>();
+  // @Input() isMonthSubmitted: boolean;
+  // @Output() isMonthSubmittedChange = new EventEmitter<boolean>();
+  // @Input() selectedMonth: Moment;
+  @Output() selectedMonthChange = new EventEmitter<Moment>();
+  // @Input() contractorName: string;
+  // @Output() contractorNameChange = new EventEmitter<string>();
 
   constructor(private scheduleService: ScheduleService) {}
 
   ngOnInit() {
-    this.initMonthPickerForm();
-    this.initContractorForm();
+    // this.initMonthPickerForm();
+    // this.initContractorForm();
+    this.initSettingsForm();
   }
 
   ngOnDestroy() {
@@ -43,35 +51,62 @@ export class ScheduleSettingsComponent implements OnInit, OnDestroy {
     this.ngUnsubscribe.complete();
   }
 
-  initMonthPickerForm(): void {
+  initSettingsForm(): void {
     this.months = this.scheduleService.getMonths(); // generates array of months to populate select's options
-    this.monthPickerForm = new FormGroup({
-      month: new FormControl({
+    this.settingsForm = new FormGroup({
+      contractorName: new FormControl(this.contractorName, Validators.required),
+      selectedMonth: new FormControl({
         value: this.months[0].firstDay,
         disabled: false
       }) // by default selects first element, first month hence
     });
   }
 
-  initContractorForm(): void {
-    this.contractorForm = new FormGroup({
-      contractorName: new FormControl(this.contractorName, Validators.required)
-    });
-
-    this.contractorForm
-      .get("contractorName")
-      .valueChanges.pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe(value => {
-        this.contractorNameChange.emit(value);
-      });
+  onFormSubmit() {
+    if (this.settingsForm.valid) {
+      this.areSettingsSubmitted = true;
+      this.settingsChange.emit(this.settingsForm.value);
+    }
   }
 
-  onSubmit() {
-    this.selectedMonthChange.emit(this.monthPickerForm.value.month); // emits choosen month from selected option
-    this.isMonthSubmittedChange.emit(true);
+  onAllowEditing() {
+    console.log("allow editing");
+    this.areSettingsSubmitted = false;
   }
 
-  onUnsubmit() {
-    this.isMonthSubmittedChange.emit(false);
-  }
+  // --- WYWALIĆ
+
+  // initMonthPickerForm(): void {
+  //   this.months = this.scheduleService.getMonths(); // generates array of months to populate select's options
+  //   this.monthPickerForm = new FormGroup({
+  //     month: new FormControl({
+  //       value: this.months[0].firstDay,
+  //       disabled: false
+  //     }) // by default selects first element, first month hence
+  //   });
+  // }
+
+  // initContractorForm(): void {
+  //   this.contractorForm = new FormGroup({
+  //     contractorName: new FormControl(this.contractorName, Validators.required)
+  //   });
+
+  //   this.contractorForm
+  //     .get("contractorName")
+  //     .valueChanges.pipe(takeUntil(this.ngUnsubscribe))
+  //     .subscribe(value => {
+  //       this.contractorNameChange.emit(value);
+  //     });
+  // }
+
+  // -------------------------
+
+  // onSubmit() {
+  //   this.selectedMonthChange.emit(this.monthPickerForm.value.month); // emits choosen month from selected option
+  //   this.isMonthSubmittedChange.emit(true);
+  // }
+
+  // onUnsubmit() {
+  //   this.isMonthSubmittedChange.emit(false);
+  // }
 }
--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -11,9 +11,17 @@ export class ScheduleComponent {
   isMonthSubmitted = false; // when month is submitted is not possible to change it
   contractorName: string;
   printMode = false;
+  settings: any;
+
+  settingsChange(settings: any) {
+    console.log("settings change", settings.month);
+    this.selectedMonth = settings.selectedMonth;
+    this.contractorName = settings.contractorName;
+  }
 
   onSelectedMonth(selectedMonth: Moment) {
-    this.selectedMonth = selectedMonth;
+    console.log("selected month", selectedMonth);
+    // this.selectedMonth = selectedMonth;
   }
 
   printModeChanged(newPrintModeStatus: boolean): void {
