199914ba7677db7bdb78e2d3eafbcf718bc123cc
Jurek <jurek2006@gmail.com>
2019-07-08 12:50:43 +0200

    fixed missing type

--- src/app/schedule/schedule-print/schedule-print.component.ts
+++ src/app/schedule/schedule-print/schedule-print.component.ts
@@ -5,24 +5,25 @@ import {
   Input,
   ViewChild,
   ElementRef
-} from "@angular/core";
-import * as jspdf from "jspdf";
-import html2canvas from "html2canvas";
-import { MomentMonthsService } from "src/app/services/moment-months.service";
-import { ScheduleDay } from "../models/scheduleDay.model";
+} from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
+import { MomentMonthsService } from 'src/app/services/moment-months.service';
+import { ScheduleDay } from '../models/scheduleDay.model';
+import { Settings } from '../models/settings.model';
 
 @Component({
-  selector: "app-schedule-print",
-  templateUrl: "./schedule-print.component.html",
-  styleUrls: ["./schedule-print.component.css"]
+  selector: 'app-schedule-print',
+  templateUrl: './schedule-print.component.html',
+  styleUrls: ['./schedule-print.component.css']
 })
 export class SchedulePrintComponent {
   @Input() schedule: ScheduleDay[];
-  @Input() settings;
+  @Input() settings: Settings;
 
   @Output() closePrint = new EventEmitter<void>();
 
-  @ViewChild("contentToConvert", { static: false }) pdfRenderView: ElementRef;
+  @ViewChild('contentToConvert', { static: false }) pdfRenderView: ElementRef;
 
   constructor(private momentMonthsService: MomentMonthsService) {}
 
@@ -46,9 +47,9 @@ export class SchedulePrintComponent {
     html2canvas(scheduleImage).then(canvas => {
       const page = { height: 297, width: 210 }; // a4 page size (in mm) to fit image on pdf page
 
-      const contentDataURL = canvas.toDataURL("image/jpeg", 0.5);
-      const pdf = new jspdf("p", "mm", "a4"); // A4 size page of PDF
-      pdf.addImage(contentDataURL, "PNG", 0, 0, page.width, page.height); // converted image occupies full A4 page
+      const contentDataURL = canvas.toDataURL('image/jpeg', 0.5);
+      const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, page.width, page.height); // converted image occupies full A4 page
       pdf.save(
         `${this.settings.contractorName} - ${this.getSelectedMonthLabel()}.pdf`
       ); // generate pdf for downloading
