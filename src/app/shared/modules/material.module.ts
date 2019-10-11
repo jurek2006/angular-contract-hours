58394008754cade075f16dea31cfa4d713da195b
Jurek <jurek2006@gmail.com>
2019-10-11 10:16:19 +0200

    added tooltip when btn Generate PDF disabled because of not matching with expected hours

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule, MatIconModule } from "@angular/material";
-import { MatInputModule } from "@angular/material/input";
-import { MatSelectModule } from "@angular/material/select";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatDividerModule } from "@angular/material/divider";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 const matModules = [
   MatButtonModule,
@@ -16,7 +17,8 @@ const matModules = [
   MatCheckboxModule,
   MatIconModule,
   MatToolbarModule,
-  MatDividerModule
+  MatDividerModule,
+  MatTooltipModule
 ];
 
 @NgModule({
