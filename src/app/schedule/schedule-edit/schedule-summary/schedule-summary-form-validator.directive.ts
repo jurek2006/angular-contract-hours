2896dda6b8a237b5c5a074f1e4f8e1b613ff4031
Jurek <jurek2006@gmail.com>
2019-10-16 07:17:36 +0200

    improved summaryForm validation and showing errors

--- src/app/schedule/schedule-edit/schedule-summary/schedule-summary-form-validator.directive.ts
+++ src/app/schedule/schedule-edit/schedule-summary/schedule-summary-form-validator.directive.ts
@@ -1,8 +1,5 @@
 import { ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms';
 
-// Validator checking (when defined total hours for month)
-// if defined (expected) total hours match current (scheduled) total hours
-// if not - returns error hoursDefinedCurrentNotMatch
 export const scheduleSummaryFormValidator: ValidatorFn = (
   form: FormGroup
 ): ValidationErrors | null => {
@@ -11,19 +8,30 @@ export const scheduleSummaryFormValidator: ValidatorFn = (
     totalHoursDefined,
     totalHoursCurrent
   } = form.controls;
+  // Validator checking (when defined total hours for month)
+  // if defined (expected) total hours match current (scheduled) total hours
+  // if not - returns error with message
   if (
     isTotalHoursDefined.value &&
     totalHoursDefined.value - totalHoursCurrent.value !== 0
   ) {
     if (totalHoursDefined.value - totalHoursCurrent.value > 0) {
       return {
-        hoursDefinedCurrentNotMatch: `Not enough hours: ${totalHoursDefined.value -
-          totalHoursCurrent.value}`
+        error: {
+          msg: `Not enough hours: ${totalHoursDefined.value -
+            totalHoursCurrent.value}`,
+          msgVerbose: `Less hours scheduled than expected: ${totalHoursDefined.value -
+            totalHoursCurrent.value}`
+        }
       };
     } else if (totalHoursDefined.value - totalHoursCurrent.value < 0) {
       return {
-        hoursDefinedCurrentNotMatch: `To many hours: ${totalHoursCurrent.value -
-          totalHoursDefined.value}`
+        error: {
+          msg: `To many hours hours: ${totalHoursCurrent.value -
+            totalHoursDefined.value}`,
+          msgVerbose: `To many hours scheduled than expected: ${totalHoursCurrent.value -
+            totalHoursDefined.value}`
+        }
       };
     }
   }
