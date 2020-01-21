501c3aaffa95a0911b330fe41699c09f00d097d2
Jurek <jurek2006@gmail.com>
2020-01-21 12:58:34 +0100

    refactor: changed to global spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { ScheduleModule } from './schedule/schedule.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [ScheduleModule, BrowserModule],
+  imports: [ScheduleModule, BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
