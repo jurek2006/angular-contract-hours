e85ff230aa086d55d550bc60776d89356371d65c
Jurek <jurek2006@gmail.com>
2020-03-13 09:33:22 +0100

    fix: not working input disable directive on Ivy
    Since Ivy AppDisableControlDirective wasn't working properly (after form initialisation).
    Thus  definedTotalHours input in the beginning was not shown properly.
    
    Closes #10

--- src/app/shared/app-disable-control.directive.ts
+++ src/app/shared/app-disable-control.directive.ts
@@ -5,9 +5,18 @@ import { NgControl } from '@angular/forms';
   selector: '[appDisableControl]'
 })
 export class AppDisableControlDirective {
-  @Input() set appDisableControl(condition: boolean) {
-    const action = condition ? 'disable' : 'enable';
-    this.ngControl.control[action]();
+  /*
+    when using Ivy (from Angular 9) there's a problem with the directive
+    (as described: https://github.com/jurek2006/angular-contract-hours/issues/10)
+    Working solution is from https://github.com/angular/angular/issues/35330
+
+  */
+  @Input() appDisableControl;
+  ngOnChanges(changes) {
+    if (changes['appDisableControl']) {
+      const action = this.appDisableControl ? 'disable' : 'enable';
+      this.ngControl.control[action]();
+    }
   }
 
   constructor(private ngControl: NgControl) {}
